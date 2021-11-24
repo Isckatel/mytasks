0282e19d64787884cfc20c0306ebb0070e96c420
Isckatel <nt-polza@yandex.ru>
2021-11-24 16:07:28 +0500

    GET /projects

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,22 +1,27 @@
 class ProjectController < ApplicationController
-    # def index
-    #     answer = {
-    #         "message" => "ok"
-    #     }
-    #     render json: answer
-    # end
-    # GET /titles
+     # GET /titles
     def index
-        # titles = Title.left_outer_joins(:tasks)
-        #                     .select('titles.id, titles.title, COUNT(tasks.id) AS tasks_count')
-        #                     .group('titles.id, titles.title')
-        titletest = Title.select("id, title")
-        puts titletest.public_methods 
-        titles = Title.joins("join tasks on titles.id = tasks.title_id").select('titles.id, titles.title, tasks.id, tasks.text, tasks."isCompleted"')
-        render json: titletest, status: :ok
+        # titles = Title.left_outer_joins(:tasks).select('titles.id, titles.title, COUNT(tasks.id) AS tasks_count').group('titles.id, titles.title')
+        # titles = Title.joins("join tasks on titles.id = tasks.title_id").select('titles.id, titles.title, tasks.id, tasks.text, tasks."isCompleted"')
+
+        # Колличество разделов
+        countTitleSQL = Title.select("COUNT(*) AS count").to_json             
+        countTitleHash = JSON.parse(countTitleSQL).first
+        countTitleHash["todos"] = "Success"  
+        countTitle = countTitleHash['count']
+
+        # Cписок разделов
+        titleList = Title.select("titles.id, titles.title").to_json
+        titleListHash = JSON.parse(titleList)
+
+        # Список задач
+        tasksHash = Hash.new
+        for i in (1..countTitle)
+            titleListHash[i-1]['todos'] = Task.select('tasks.id, tasks.text, tasks."isCompleted"').where(title_id:i).as_json
+        end        
+        # test << { :status => "Success" }
+        # puts titletest.public_methods
+        render json: titleListHash, status: :ok
     end
-    # Category.left_outer_joins(:products)
-    #                         .select('categories.id, categories.name, COUNT(products.id) AS products_count')
-    #                         .where(id: id)
-    #                         .group('categories.id, categories.name')
+
 end
