eace59e85c9ea135cc2acd67413edf40a74ce427
Isckatel <nt-polza@yandex.ru>
2021-11-27 20:59:59 +0500

    fixing routesre

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "project#index"
 
-  get "/projects", to: "project#index"
-  get "/projects/:title_id/todo/:id", to: "project#edit"
+  get "/projects", to: "project#index" 
   patch "/projects/:title_id/todo/:id", to: "project#update"
   post "/todos", to: "project#new" 
 
