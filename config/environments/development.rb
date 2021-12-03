7b0b9debcae7aa42efb84a78726b20f2077e47cf
Isckatel <nt-polza@yandex.ru>
2021-12-03 13:17:26 +0500

    config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #Снять блокировку
+  config.hosts << /[a-z0-9]+\.herokuapp\.com/
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
