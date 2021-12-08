2cc9e91a8009c8760c1ea5ae7378159fb78d5e2a
Isckatel <nt-polza@yandex.ru>
2021-12-08 11:09:53 +0500

    micro fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,4 +9,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #http://127.0.0.1:4200
   Rails.application.config.hosts << "mytasks-ruby.herokuapp.com"
   Rails.application.config.hosts << "blooming-dawn-85383.herokuapp.com"
-  Rails.application.config.hosts << /[a-z0-9]+\.herokuapp\.com/git 
+  Rails.application.config.hosts << /[a-z0-9]+\.herokuapp\.com/
